37136e92daa7d11098f068b4e8000b8253cfd03f
Walter <lavoing@gmail.com>
2021-12-03 16:50:56 -0500

    Artifact_Instance and Contributor models

--- inheritance_project/inheritance/models.py
+++ inheritance_project/inheritance/models.py
@@ -1,9 +1,14 @@
 
+# Required for unique artiface instances.  Example, black and white filtering, sepia_tone, and color.  Creates unique id for artifact.
+import uuid
 from django.db import models
+from django.db.models.deletion import RESTRICT
 
 # Create your models here.
 
-from django.urls import reverse  # To generate URLS by reversing URL patterns
+from django.urls import reverse
+
+# To generate URLS by reversing URL patterns
 
 
 class artifacts(models.Models):
@@ -19,3 +24,35 @@ class artifacts(models.Models):
 
 def _str_(self):
     return self.artifacts
+
+
+def get_absolute_url(self):
+    # Returns the url to access a detail record for the artifact.
+
+    return reverse("artifacts_detail", kwargs={"pk": self.pk})
+
+
+class artifact_instance(models.Model):
+    black_and_white = models.CharField(max_length=50)
+    sepia_tone = models.CharField(max_length=50)
+    color_artifact = models.CharField(max_length=50)
+    artifact = models.ForeignKey(
+        'Artifact', on_delete=models.RESTRICT, null=True)
+
+
+def __str__(self):
+    return f'uuid'.artifact_instance
+
+
+class contributor(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    def get_absolute_url(self):
+        # Returns the url to access a particular author instance
+        return reverse("contributor_detail", kwargs={"pk": self.pk})
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
