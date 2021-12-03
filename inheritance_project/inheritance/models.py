4bf1ddf31da4f9e958eae37a337790edc681a17f
Walter <lavoing@gmail.com>
2021-12-03 11:11:50 -0500

    changes 12.3.21

--- inheritance_project/inheritance/models.py
+++ inheritance_project/inheritance/models.py
@@ -1,3 +1,21 @@
+
 from django.db import models
 
 # Create your models here.
+
+from django.urls import reverse  # To generate URLS by reversing URL patterns
+
+
+class artifacts(models.Models):
+    artifact_title = models.CharField(
+        max_length=50, help_text='Enter name of Artifact')
+    artifact_summary = models.CharField(
+        max_length=250, help_text='Enter description of Artifact')
+    inheritance_date = models.DateField()
+    contributor = models.CharField(max_length=50, help_text='Enter your name')
+    artifact_image = BLOB
+    access_key = ()  # access_key assignment to be determined
+
+
+def _str_(self):
+    return self.artifacts
