2b82f04d6e7c819025982348fd6f24d12b18abdd
Walter <lavoing@gmail.com>
2021-12-09 14:19:22 -0500

    Edits to commented code work

--- inheritance_project/inheritance/forms.py
+++ inheritance_project/inheritance/forms.py
@@ -1,2 +1,8 @@
 #from django import forms
-#from .models import models
+#from .models import *
+
+# class Artifact_form(forms.ModelForm):
+
+# class Meta:
+#model = Artifact
+#fields = ['artifact_img']
