eb4dc8574a9fd0ba268adcffefb201a672b5dce8
Walter <lavoing@gmail.com>
2021-12-08 20:34:37 -0500

    establishing new branch

--- inheritance_project/inheritance/urls.py
+++ inheritance_project/inheritance/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.landingpage, name='landingpage'),
+    path('landingpage', views.landingpage, name='landingpage'),
     path('ourstory', views.ourstory, name='ourstory'),
     path('addartifact', views.addartifact, name='addartifact'),
     path('gallery', views.gallery, name='gallery'),
